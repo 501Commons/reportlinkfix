4d1011bf6d5ded18392043721328f1ee04948caf
brett <brett@501commons.org>
2022-01-18 12:14:24 -0800

    Prevent the terminal window from closing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 use ::std::*;
+use std::io::{stdin, stdout, Read, Write};
+
+fn pause() {
+    // Keeps the terminal window open
+    let mut stdout = stdout();
+    stdout.write(b"Press Enter to close...").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
+}
 
 fn main() {
     let mut original_link = String::new();
     println!("Input the N-Central Report Manager Link:");
+    // Read the original link from the terminal
     io::stdin()
         .read_line(&mut original_link)
         .expect("Error reading input");
+    // Remove any space at the beginning or the end of the link
     let trimmed_link = original_link.trim();
+    // Check whether the link is valid and if it's URL encoded. Then generate the new link
     if trimmed_link.starts_with("javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1', '") {
-        // Firefox original_link javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1', '1917965022');
+        // Not URL encoded original_link example javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1', '0123456789');
         let link_trim_start = trimmed_link.trim_start_matches("javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1', '");
         let new_link = link_trim_start.trim_end_matches("');");
-        println!("Firefox Link");
         println!("http://501c-reportmgr.npowerseattle.lan/ReportInterface/Pages/menu.aspx?SessionID={}&DataSourceID=1", new_link);
     } else if trimmed_link.starts_with("javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1%27,%20%27") {
-        // Chrome/Edge original_link javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1%27,%20%272076719063%27);
+        // URL encoded original_link example javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1%27,%20%270123456789%27);
         let link_trim_start = trimmed_link.trim_start_matches("javascript:javascript:ui.root.externalPopupWindowPostToReportManager('http://501C-REPORTMGR.npowerseattle.lan/ReportInterface/Pages/menu.aspx?DataSourceID=1%27,%20%27");
         let new_link = link_trim_start.trim_end_matches("%27);");
-        println!("Chrome/Edge Link");
         println!("http://501c-reportmgr.npowerseattle.lan/ReportInterface/Pages/menu.aspx?SessionID={}&DataSourceID=1", new_link);
     } else {
         println!("Not a valid link");
     }
+    // Keep the terminal window open
+    pause();
 }
